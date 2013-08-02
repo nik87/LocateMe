8c6278ef58aa5eda2a1537070d9adf56bceebdcd
nik87 <partylunzer@partyheld.de>
2013-08-02 10:58:44 +0200

    Edit klasse weiter programmiert und Menu.java weiter programmiert

--- src/com/nik/locateme/Edit.java
+++ src/com/nik/locateme/Edit.java
@@ -1,22 +1,63 @@
 package com.nik.locateme;
 
-import android.os.Bundle;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Edit extends Activity {
+public class Edit extends Activity implements OnClickListener{
 
+	private Button save;
+	private EditText description, street, hNumber, postcode;
+	private String DESCRIPTION, STREET, HNUMBER, POSTCODE,DATA;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.edit);
+		save = (Button) findViewById(R.id.btn_saveAdress);
+		description = (EditText) findViewById(R.id.description);
+		street = (EditText) findViewById(R.id.streetname);
+		hNumber = (EditText) findViewById(R.id.hNumb);
+		postcode = (EditText) findViewById(R.id.postcode);
 	}
 
+	/**
+	 * Soll die eingetragenden Daten abspeichern
+	 */
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.edit, menu);
-		return true;
+	public void onClick(View arg0) {
+		
+		DESCRIPTION = description.getText().toString();
+		STREET = street.getText().toString();
+		HNUMBER = hNumber.getText().toString();
+		POSTCODE = postcode.getText().toString();
+		
+		//DATA.append(STREET).append(" ").append(HNUMBER).append(POSTCODE);
+		
+		DATA  = STREET + " " + HNUMBER + " " + POSTCODE;
+		
+		
+		try {
+			FileOutputStream fos = openFileOutput(DESCRIPTION, Context.MODE_PRIVATE);
+			fos.write(DATA.getBytes());
+			fos.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/com/nik/locateme/Menu.java
+++ src/com/nik/locateme/Menu.java
@@ -1,23 +1,27 @@
 package com.nik.locateme;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.DateKeyListener;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.DatePicker.OnDateChangedListener;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,8 +29,9 @@ public class Menu extends Activity implements OnClickListener {
 
 	private DatePicker dppickerDate;
 	private TextView tvDisplayDate;
-	private Button btnChangeDate;
-
+	private Button btnChangeDate, btnSave;
+	private Spinner spSelectAdress;
+	
 	private int year;
 	private int month;
 	private int day;
@@ -37,15 +42,33 @@ public class Menu extends Activity implements OnClickListener {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.menu);
-
+		spSelectAdress = (Spinner) findViewById(R.id.spAdress);
+		btnSave = (Button) findViewById(R.id.btnSave);
+		
 		setCurrentDateOnView();
 		addListenerButton();
+		getAdress();
+	}
+	/**
+	 * soll sich die eingtragenden Adressen holen 
+	 */
+	private void getAdress() {
+		// TODO Auto-generated method stub
+		String[] adress = getApplicationContext().fileList();
+		List<String> list = new ArrayList<String>();
+		for(int i = 0; i<adress.length; i++){
+			//Log.d("Filename", filenames[i]);
+			list.add(adress[i]);
+		}
+		ArrayAdapter<String> filenameAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, list);
+		spSelectAdress.setAdapter(filenameAdapter);
 	}
 
 	private void setCurrentDateOnView() {
 		// TODO Auto-generated method stub
 		tvDisplayDate = (TextView) findViewById(R.id.info);
 		dppickerDate = (DatePicker) findViewById(R.id.pickerdate);
+		
 
 		final Calendar calendar = Calendar.getInstance();
 		year = calendar.get(Calendar.YEAR);
@@ -54,8 +77,8 @@ public class Menu extends Activity implements OnClickListener {
 
 		// Datum in Textview
 
-		tvDisplayDate.setText(new StringBuilder().append(month + 1).append("-")
-				.append(day).append("-").append(year).append(" "));
+		//tvDisplayDate.setText(new StringBuilder().append(month + 1).append("-")
+			//	.append(day).append("-").append(year).append(" "));
 
 		dppickerDate.init(year, month, day, null);
 	}
@@ -110,38 +133,38 @@ public class Menu extends Activity implements OnClickListener {
 		}
 	};
 
+	/**
+	 * soll sich beim drücken die Daten aus dem STream holen und im Spinner anzeigen
+	 */
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		
-		FileOutputStream out = null;
-		OutputStreamWriter writer = null;
-
-		try {
-			out = openFileOutput("Daten.csv", MODE_ENABLE_WRITE_AHEAD_LOGGING);
-			writer = new OutputStreamWriter(out);
-			writer.write("'das hier' ist wichtig");
-			writer.flush();
-			Toast.makeText(this, "Datei wurde gesperichert", Toast.LENGTH_SHORT).show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (writer != null)
-					writer.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			try {
-				if (out != null)
-					out.close();
-			} catch (Exception e) {
-				// TODO: handle exception
-				e.printStackTrace();
-			}
-		}
+		String selectedItem = String.valueOf(spSelectAdress.getSelectedItem());
+		openFile(selectedItem);
 	}
-
+	private void openFile(String selectedFile) {
+		// TODO Auto-generated method stub
+		String value = "";
+		FileInputStream fis;
+		
+		try {
+			fis = openFileInput(selectedFile);
+			byte[] input = new byte[fis.available()];
+		while (fis.read(input) != -1) {
+			value += new String(input);
+		}
+		fis.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		tvDisplayDate.setText(value);
+		
+	}
 	public boolean onCreateOptionsMenu(android.view.Menu menu){
 		super.onCreateOptionsMenu(menu);
 		
